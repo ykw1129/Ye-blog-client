8a82ecc3a560a5093ac074dbe599581d9d927b8e
ykw <1794093413@qq.com>
2021-03-12 13:33:18 +0800

    🎉 初始化

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <p>123</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
+
+<style lang="scss" scoped>
+
+p{
+  color: red;
+}
+</style>
